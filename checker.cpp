344dd3b98e23f207166dbeb6dff8bae94468c4bf
bharathgattu <134360299+bharathgattu@users.noreply.github.com>
2023-08-30 22:30:58 +0530

    draft4-reduceComplexity

--- checker.cpp
+++ checker.cpp
@@ -7,39 +7,47 @@ void printPattern() {
     for (int i = 0; i < 6; i++)
     {
         cout << "\r* " << flush;
-        sleep(1);
-        cout << "\r *" << flush;
-        sleep(1);
+         sleep(1);
+         cout << "\r *" << flush;
+         sleep(1);
     }
 }
 
 bool isTemperatureCritical(float temperature) {
-    return (temperature > 102 || temperature < 95);
+    bool result = false;
+    result = temperature > 102 || temperature < 95;
+    if (result == true) {
+        cout << "Temperature critical!\n";
+        printPattern();
+    }
+
+    return result;
 }
 
 bool isPulseRateWithinRange(float pulseRate) {
-    return (pulseRate < 60 || pulseRate > 100);
+    bool result = false;
+    result = pulseRate < 60 || pulseRate > 100;
+
+    if (result == true) {
+        cout << "Pulse Rate is out of range!\n";
+        printPattern();
+    }
+
+    return result;
 }
 
 bool isSpo2LevelOK(float spo2) {
-    return (spo2 < 90);
-}
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-    bool result = true;
-    if (isTemperatureCritical(temperature) == true) {
-        cout << "Temperature critical!\n";
-        result = false;
-    }
-    else if (isPulseRateWithinRange(pulseRate)) {
-        cout << "Pulse Rate is out of range!\n";
-        result = false;
-    }
-    else if (isSpo2LevelOK(spo2)) {
+    bool result = false;
+    result = spo2 < 90;
+    if (result == true) {
         cout << "Oxygen Saturation out of range!\n";
-        result = false;
+        printPattern();
     }
-    printPattern();
+
     return result;
+}
+bool vitalsOk(float temperature, float pulseRate, float spo2) {
+    return  !(isTemperatureCritical(temperature) || isPulseRateWithinRange(pulseRate) || isSpo2LevelOK(spo2));
 }
 
 int main() {
